098a9ab53670b8760490456fc4f9df238d1f7daa
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-12-14 09:42:21 -0500

    refactor: restructure App component and add Board component for Tic-Tac-Toe game

---
+++ Tic-Tac-Toe/tic-tac-toe/src/components/Board.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Board: React.FC = () => {
+  return (
+    <div className = "board">
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+        <div className ="square"></div>
+    </div>
+  );
+}
---
+++ Tic-Tac-Toe/tic-tac-toe/src/components/Square.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Square: React.FC = () => {
+  return (
+    <div className ="square"></div>
+  )
+};
