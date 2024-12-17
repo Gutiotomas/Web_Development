9ada6d40d892a5783928171286089458f91da180
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-12-16 20:21:34 -0500

    feat: implement Board component with player toggle and state management

--- Tic-Tac-Toe/tic-tac-toe/src/components/Board.tsx
+++ Tic-Tac-Toe/tic-tac-toe/src/components/Board.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { Square } from "./Square.tsx";
+import { BoardCells } from "../types";
 
 export const Board: React.FC = () => {
+  const [squares, setSquares] = useState<BoardCells>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i: number) => {
+    squares[i] = xIsNext ? "X" : "O";
+    setSquares();
+    setXIsNext(!xIsNext);
+  };
+
   return (
-    <div className = "board">
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-        <div className ="square"></div>
-    </div>
+    <>
+      <h1>The next player is {xIsNext ? "X" : "O"}</h1>
+      <div className="board">
+        <div className="square">{squares[0]}</div>
+        <div className="square">{squares[1]}</div>
+        <div className="square">{squares[2]}</div>
+        <div className="square">{squares[3]}</div>
+        <div className="square">{squares[4]}</div>
+        <div className="square">{squares[5]}</div>
+        <div className="square">{squares[6]}</div>
+        <div className="square">{squares[7]}</div>
+        <div className="square">{squares[9]}</div>
+      </div>
+    </>
   );
-}
+};
