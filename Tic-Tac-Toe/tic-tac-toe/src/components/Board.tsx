794f3f314a5054d6f78cbfe0d5cc9d04104b7c59
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-12-16 20:25:55 -0500

    fix: update handleClick function and add onClick handlers for board squares

--- Tic-Tac-Toe/tic-tac-toe/src/components/Board.tsx
+++ Tic-Tac-Toe/tic-tac-toe/src/components/Board.tsx
@@ -8,8 +8,7 @@ export const Board: React.FC = () => {
   const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = (i: number) => {
-    squares[i] = xIsNext ? "X" : "O";
-    setSquares();
+    setSquares(squares.with(i, xIsNext ? "X" : "O"));
     setXIsNext(!xIsNext);
   };
 
@@ -17,15 +16,33 @@ export const Board: React.FC = () => {
     <>
       <h1>The next player is {xIsNext ? "X" : "O"}</h1>
       <div className="board">
-        <div className="square">{squares[0]}</div>
-        <div className="square">{squares[1]}</div>
-        <div className="square">{squares[2]}</div>
-        <div className="square">{squares[3]}</div>
-        <div className="square">{squares[4]}</div>
-        <div className="square">{squares[5]}</div>
-        <div className="square">{squares[6]}</div>
-        <div className="square">{squares[7]}</div>
-        <div className="square">{squares[9]}</div>
+        <div className="square" onClick={() => handleClick(0)}>
+          {squares[0]}
+        </div>
+        <div className="square" onClick={() => handleClick(1)}>
+          {squares[1]}
+        </div>
+        <div className="square" onClick={() => handleClick(2)}>
+          {squares[2]}
+        </div>
+        <div className="square" onClick={() => handleClick(3)}>
+          {squares[3]}
+        </div>
+        <div className="square" onClick={() => handleClick(4)}>
+          {squares[4]}
+        </div>
+        <div className="square" onClick={() => handleClick(5)}>
+          {squares[5]}
+        </div>
+        <div className="square" onClick={() => handleClick(6)}>
+          {squares[6]}
+        </div>
+        <div className="square" onClick={() => handleClick(7)}>
+          {squares[7]}
+        </div>
+        <div className="square" onClick={() => handleClick(8)}>
+          {squares[8]}
+        </div>
       </div>
     </>
   );
